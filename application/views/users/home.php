b139ecd7a4d830a604c240b7d4e4c08f50ba33a0
Mahmud Khaerulumam Kurniawan <deni.victoria@gmail.com>
2023-03-28 03:22:39 +0000

    add .env.example and fix script load in views/users/home.php

--- application/views/users/home.php
+++ application/views/users/home.php
@@ -112,8 +112,8 @@
 
 
 <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?=base_url('assets/')?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?=base_url('assets/')?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
 <script>
   function inputAngka(evt){
